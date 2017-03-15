7b9e3901e18186b75418587424566e583aa5703a
Henrique Rangel <hrangel@nucleo235.com.br>
2017-03-15 16:21:21 -0300

    corrigidos problemas com tag vazia

--- app/helpers/inplace_editing_helper.rb
+++ app/helpers/inplace_editing_helper.rb
@@ -99,9 +99,10 @@ module InplaceEditingHelper
 
   def empty_content_tag(*args)
     if block_given?
-      tag = Tag.new(args[0], args[1] || {})
+      tag = EmptyTag.new(args[0], args[1] || {})
       old_buf = @output_buffer
       @output_buffer = ActionView::OutputBuffer.new
+      yield(tag)
       content = tag.render(@output_buffer.presence)
       @output_buffer = old_buf
       content
@@ -131,6 +132,67 @@ module InplaceEditingHelper
     
     def render(content)
       "<#{name}#{render_attributes}>#{content ? content.strip : nil}</#{name}>".html_safe
+    end
+
+    def render_attributes
+      attrs = @attributes.dup
+      if self[:class].empty?
+        attrs.delete :class
+      else
+        attrs[:class] = self[:class].to_s
+      end
+
+      attrs.keys.map do |k|
+        "#{k}='#{ERB::Util.html_escape(attrs[k])}'".html_safe
+      end.join(' ').prepend(' ')
+    end
+
+    class CSS
+      
+      def initialize(css)
+        if css.is_a? String
+          @internals = css.split(' ')
+        else
+          @internals = css.to_a
+        end
+      end
+
+      def to_s
+        @internals.uniq.join(' ')
+      end
+
+      def empty?
+        @internals.empty?
+      end
+
+      def <<(name)
+        @internals << name
+        nil
+      end
+    end
+  end
+
+  class EmptyTag
+    include ActionView::Helpers::CaptureHelper
+    attr_accessor :id
+    attr_reader :name, :css
+
+    def initialize(name, attributes = {})
+      @name = name
+      @attributes = attributes.with_indifferent_access
+      @attributes[:class] = Tag::CSS.new(attributes[:class])
+    end
+
+    def []=(k,v)
+      @attributes[k] = v
+    end
+    
+    def [](k)
+      @attributes[k]
+    end
+    
+    def render(content)
+      "<#{name}#{render_attributes} />".html_safe
     end
 
     def render_attributes
